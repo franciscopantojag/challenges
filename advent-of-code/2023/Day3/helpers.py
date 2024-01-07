ecee3d80b818fea2bef795f18df723dd1b8c8d21
Francisco Pantoja <franciscopantojag98@gmail.com>
2024-01-07 16:41:01 -0500

    finish day3

--- advent-of-code/2023/Day3/helpers.py
+++ advent-of-code/2023/Day3/helpers.py
@@ -1,19 +1,94 @@
+from curses.ascii import isdigit
+from functools import reduce
 import re
+from os import path
+
+input_path = path.join(path.dirname(__file__), 'input.txt')
 
 
-def has_non_numeric_or_dot_chars(input_string: 'str'):
-    return bool(re.search(r'[^0-9.]', input_string))
+def complete_num_given_pos(symb_col: 'int', line: 'str'):
+    if not ((symb_col >= 0) and (symb_col <= len(line) - 1)):
+        return
+
+    num_str = line[symb_col]
+    if not num_str.isdigit():
+        return
+
+    idx = symb_col + 1
+
+    while idx <= (len(line) - 1):
+        curr_char = line[idx]
+        if curr_char.isdigit():
+            num_str += curr_char
+        else:
+            break
+        idx += 1
+
+    idx = symb_col - 1
+
+    while idx >= 0:
+        curr_char = line[idx]
+        if curr_char.isdigit():
+            num_str = f'{curr_char}{num_str}'
+
+        else:
+            break
+        idx -= 1
+
+    return int(num_str)
 
 
-def is_index_inside(st: 'str|list[str]', index: 'int'):
-    return index >= 0 and index <= len(st) - 1
+def get_nums_for_columns(cols: 'list[int]', line: 'str'):
+    res_raw = [complete_num_given_pos(col, line) for col in cols]
+    result = [num for num in res_raw if num != None]
+
+    return result
 
 
-def get_top_or_bottom_comparison(prev_or_next_line: 'str', start: 'int', end: 'int'):
-    start_pos = max(start - 1, 0)
+def get_nums_for_line(symb_col: 'int', line: 'str'):
+    cols = [symb_col]
+    if not line[symb_col].isdigit():
+        cols += [symb_col + 1, symb_col - 1]
 
-    end_pos_raw = end + 1
-    end_pos = end_pos_raw if is_index_inside(
-        prev_or_next_line, end_pos_raw) else end
+    return get_nums_for_columns(cols, line)
 
-    return prev_or_next_line[start_pos: end_pos]
+
+def get_results():
+    result = 0
+    gear_ratios_sum = 0
+
+    with open(input_path) as f:
+        lines = f.readlines()
+        for row, line in enumerate(lines):
+            line = line.strip()
+
+            line_symbol_matches = re.finditer(r'[^0-9.]', line)
+            for symb in line_symbol_matches:
+                symb_col = symb.start()
+
+                lines_to_process = [line]
+                if row > 0:
+                    # we have a prev line
+                    lines_to_process.append(lines[row - 1])
+
+                has_next_line = (row + 1) <= (len(lines) - 1)
+                if has_next_line:
+                    lines_to_process.append(lines[row + 1])
+
+                nums_for_this_symbol = reduce(
+                    lambda acc, line_to_process: acc + get_nums_for_line(
+                        symb_col, line_to_process),
+                    lines_to_process,
+                    []
+                )
+
+                result += sum(nums_for_this_symbol)
+
+                gear_ratio = 0
+                if len(nums_for_this_symbol) == 2:
+                    gear_ratio = nums_for_this_symbol[0] * \
+                        nums_for_this_symbol[1]
+
+                gear_ratios_sum += gear_ratio
+
+    return result, gear_ratios_sum
--- advent-of-code/2023/Day3/solution.py
+++ advent-of-code/2023/Day3/solution.py
@@ -1,61 +1,17 @@
-from os import path
-import re
-from helpers import is_index_inside, get_top_or_bottom_comparison, has_non_numeric_or_dot_chars
-
-input_path = path.join(path.dirname(__file__), 'input.txt')
+from helpers import get_results
 
 
 def part_one():
-    result = 0
-    with open(input_path) as f:
-        lines = f.readlines()
-        for row, line in enumerate(lines):
-            matches = re.finditer('\\d+', line)
+    return get_results()[0]
 
-            for match_obj in matches:
-                num = int(match_obj.group())
-                num_pos_start = match_obj.start()
-                num_pos_end = match_obj.end()
 
-                str_compare = ''
-
-                # prev row compare
-                prev_row_num = row - 1
-                has_prev_row = is_index_inside(lines, prev_row_num)
-
-                if has_prev_row:
-                    prev_line = lines[prev_row_num]
-                    str_compare = get_top_or_bottom_comparison(
-                        prev_line, num_pos_start, num_pos_end)
-
-                # left char compare
-                char_left_col = num_pos_start - 1
-                if is_index_inside(line, char_left_col):
-                    str_compare += line[char_left_col]
-
-                # right char compare
-                if is_index_inside(line, num_pos_end):
-                    str_compare += line[num_pos_end]
-
-                # next row compare
-                next_row_num = row + 1
-                has_next_row = is_index_inside(lines, next_row_num)
-
-                if has_next_row:
-                    next_line = lines[next_row_num]
-                    str_compare += get_top_or_bottom_comparison(
-                        next_line, num_pos_start, num_pos_end)
-
-                str_compare = str_compare.replace('\n', '')
-
-                if has_non_numeric_or_dot_chars(str_compare):
-                    result += num
-
-    return result
+def part_two():
+    return get_results()[1]
 
 
 def main():
     print(part_one())  # 535235
+    print(part_two())  # 79844424
 
 
 if __name__ == '__main__':
